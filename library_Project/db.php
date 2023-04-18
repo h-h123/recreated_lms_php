c656de05649be04aa81e56afc456ba6089199717
Hunain <hunainkhan142001@gmail.com>
2023-04-18 13:22:58 +0530

    azuremysql_connection

--- library_Project/db.php
+++ library_Project/db.php
@@ -1,16 +1,33 @@
 <?php
-class db{
-protected $connection;
+// class db{
+// protected $connection;
 
-function setconnection(){
-    try{
-        $this->connection=new PDO("mysql:host=; dbname=library_managment","root","");
-        // echo "Done";
-    }catch(PDOException $e){
-        echo "Error";
-        //die();
+// function setconnection(){
+//     try{
+//         $this->connection=new PDO("mysql:host=; dbname=library_managment","root","");
+//         // echo "Done";
+//     }catch(PDOException $e){
+//         echo "Error";
+//         //die();
 
-    }
+//     }
+// }
+
+// }
+
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:php1server.database.windows.net,1433; Database = phpdb", "php1server", "Azure123456789");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
-}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "php1server", "pwd" => "Azure123456789", "Database" => "phpdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:php1server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
